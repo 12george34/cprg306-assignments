ecccc8dc04f781a15ae3ee3ae7167b56d2d6d084
12george34 <georgech8765@gmail.com>
2026-01-20 16:04:31 -0700

    fest push

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import StudentInfo from "./week-2/student-info";
+
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <a href="./week-2">week-2</a>
+    </main>
+  );
+}
+
+
