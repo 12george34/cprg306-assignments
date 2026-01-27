ea3d7c3fa6fae6d6eab48248b8aaf8aa0bc1b7d9
12george34 <georgech8765@gmail.com>
2026-01-27 15:36:08 -0700

    week3b

--- app/week-3/item.tsx
+++ app/week-3/item.tsx
@@ -7,7 +7,7 @@ interface Things  {
 
 
 const Items = ({name = "George", quantity = 4, category = "breakfast"}: Things) => {
-    return (<div className="border border-red-500 m-2 p-2 flex items-center justify-center ">
+    return (<div className="border border-red-500 m-2 p-2 flex items-center justify-center">
         <li className="text-orange-500 ">{name}, {quantity}, {category}</li></div>
         
     )
